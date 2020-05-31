6b111ad9f365821133e025039ab2edfa9310c19e
Marwa64 <46577944+Marwa64@users.noreply.github.com>
2020-05-31 22:15:55 +0200

    Add files via upload

--- slot.java
+++ slot.java
@@ -1,10 +1,15 @@
-
+/**
+ * <h1>The Slot class</h1>
+ * <p> 	This class represents the slot that the player can book
+ */
 public class slot {
 	
 	private String from, to, date;
 	player player;
 	private boolean available;
-	
+	/**
+	 * Default Constructor that initializes all the attributes
+	 */
 	public slot(String from, String to, String date){
 		this.from = from;
 		this.to= to;
@@ -12,28 +17,52 @@ public class slot {
 		available = true;
 		player = null;
 	}
+	/**
+	 * The book method changes available to false
+	 */
 	public void book() {
 		available = false;
 	}
+	/**
+	 * The unBook method changes available to true and sets the player to null
+	 */
 	public void unBook() {
 		available = true;
 		player = null;
 	}
+	/**
+	 * @return the opposite of the available attribute
+	 */
 	public boolean isBooked() {
 		return !(available);
 	}
+	/**
+	 * @return the time the slot begins at
+	 */
 	public String getFrom() {
 		return from;
 	}
+	/**
+	 * @return the time the slot ends at
+	 */
 	public String getTo() {
 		return to;
 	}
+	/**
+	 * @return the date of the slot
+	 */
 	public String getDate() {
 		return date;
 	}
+	/**
+	 * @param player is the player that booked this slot
+	 */
 	public void setPlayer(player player) {
 		this.player = player;
 	}
+	/**
+	 * @return the player that booked this slot
+	 */
 	public player getPlayer() {
 		return player;
 	}
